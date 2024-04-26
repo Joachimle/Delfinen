d628895edf2a29f209fca225a24cc61e0c940b7f
zako0001 <zako0001@stud.kea.dk>
2024-04-26 10:56:09 +0200

    test tilføjet

---
+++ src/test/java/TestEksempel.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestEksempel {
+
+    @DisplayName("Test eksempel")
+    @Test
+    void testEksempel() {
+        assertEquals(true, true);
+    }
+}
