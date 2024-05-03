c2803f8832e19bbe70f1c8f24d9443045d64ecb8
Joachimle <joel0002@stud.kea.dk>
2024-05-03 10:06:48 +0200

    Tilføjet kontingent-beregner for enkelte medlemmers kontingent i Medlem + summering af kontingenter i Klub

--- src/main/java/domain_model/Klub.java
+++ src/main/java/domain_model/Klub.java
@@ -6,9 +6,17 @@ public class Klub {
     private ArrayList<Medlem> medlemmer = new ArrayList<>();
 
 
-
+    /////// METHODS ////////
     public void tilføjMedlem(String navn, int månedsdag, int måned, int år, boolean aktivtMedlem) {
         medlemmer.add(new Medlem(navn, månedsdag, måned, år, aktivtMedlem));
+    }
+
+    public int sumAfKontingent(){
+        int sum = 0;
+        for (Medlem medlem : medlemmer) {
+            sum += medlem.kontingentPris();
+        }
+        return sum;
     }
 
 }
--- src/main/java/domain_model/Medlem.java
+++ src/main/java/domain_model/Medlem.java
@@ -1,6 +1,7 @@
 package domain_model;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 public class Medlem {
     //////// ATTRIBUTES /////////
@@ -14,5 +15,21 @@ public class Medlem {
         this.navn = navn;
         this.fødselsdato = LocalDate.of(år, måned, månedsdag);
         this.aktivtMedlem = aktivtMedlem;
+    }
+
+    ///////// Methods ////////
+
+    public int kontingentPris(){
+        int antalÅrGammel = Period.between(fødselsdato, LocalDate.now()).getYears();
+
+        if(!aktivtMedlem){
+            return 500;
+        } else if (antalÅrGammel < 18) {
+            return 1000;
+        } else if (antalÅrGammel > 60) {
+            return 1200;
+        } else {
+            return 1600;
+        }
     }
 }
